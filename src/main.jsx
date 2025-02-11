36aed5f1496e3eec1e552e9c1efeed08cc69e2a8
Awill Jay Macalisang <awill@SYNTACTICS.LOCAL>
2025-02-11 14:22:57 +0800

    Feb 11 Updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Homepage from './routes/Homepage.jsx';
+import PostListPage from './routes/PostListPage.jsx';
+import Write from './routes/Write.jsx';
+import LoginPage from './routes/LoginPage.jsx';
+import RegisterPage from './routes/RegisterPage.jsx';
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+
+const root = document.getElementById("root");
+
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>  
+      <Route element={<MainLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="posts" element={<PostListPage />} />
+        <Route path=":slug" element={<SinglePostPage />} />
+        <Route path="write" element={<Write />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
